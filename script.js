71a3ce18bd371f596bc63954a1ede0585a0dee7f
Gunjan Bhanarkar <82956298+gunjan1909@users.noreply.github.com>
2021-09-08 12:08:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const toggle = document.getElementById("toggle");
+const sidebar = document.getElementById("sidebar");
+
+document.onclick = function (e) {
+  if (e.target.id !== "sidebar" && e.target.id !== "toggle") {
+    sidebar.classList.remove("active");
+    toggle.classList.remove("active");
+  }
+};
+
+toggle.onclick = function () {
+  toggle.classList.toggle("active");
+  sidebar.classList.toggle("active");
+};
